a3eab67b47f6bc79b823e4054f88cde2ea454101
Henry Blanchette <henry@rybl.net>
2025-08-16 11:00:53 -0400

    [checkpoint]

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,11 +5,6 @@ import env from "./env";
 export const middleware = paymentMiddleware(
     env.RECEIVER_ADDRESS,
     {
-        "/paid": {
-            price: "$0.001",
-            network,
-            config: { description: "Access to paid content" },
-        },
         "/paid/post/*": {
             price: "$0.001",
             network,
@@ -20,7 +15,7 @@ export const middleware = paymentMiddleware(
         url: publicFacilitatorUrl,
     },
     {
-        appName: "Next x402 Demo",
+        appName: "forohtoo",
         appLogo: "/favicon.png",
     },
 );
