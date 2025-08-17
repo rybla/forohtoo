2f51db7d323582ba182e32e640a508f52095df7b
Henry Blanchette <henry@rybl.net>
2025-08-16 21:26:30 -0400

    fixed up header

--- packages/forohtoo-blog/src/component/Header.tsx
+++ packages/forohtoo-blog/src/component/Header.tsx
@@ -5,13 +5,11 @@ import styles from "./Header.module.css";
 export default function Header(props: { subtitle: string }) {
     return (
         <div className={styles.Header}>
-            <div className={styles.title}>
-                <Link href="/">{appName}</Link>
-            </div>
-            <div className={styles.separator}>{"|"}</div>
-            <div className={styles.subtitle}>{props.subtitle}</div>
+            <div className={styles.title}><Link href="/">{appName}</Link></div>
             <div className={styles.separator}>{"|"}</div>
             <div className={styles.subtitle}><Link href="/about">about</Link></div>
+            <div className={styles.separator}>{"|"}</div>
+            <div className={styles.subtitle}>{props.subtitle}</div>
         </div>
     );
 }
